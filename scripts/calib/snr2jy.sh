22c53a9b1ece2d1306e8c0b86ca933734640b89e
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-02-13 11:47:06 +0800

    comments added and converged to use SNR as is and multiply by Sigma_sim to calculate peak flux density in units of Jy

--- scripts/calib/snr2jy.sh
+++ scripts/calib/snr2jy.sh
@@ -10,4 +10,23 @@ if [[ -n "$2" && "$2" != "-" ]]; then
    sefd=$2
 fi
 
-cat ${files} | grep -v "#" | awk -v sefd=${sefd} -v npol=2 -v bw_hz=31250000 -v timeres_sec=0.001 '{snr=$2;sigma_sim=sefd/sqrt(npol*bw_hz*timeres_sec);flu=snr*sigma_sim;print snr" -> "flu;}'
+# was 31250000 0.001
+# cat ${files} | grep -v "#" | awk -v sefd=${sefd} -v npol=2 -v bw_hz=29629629 -v timeres_sec=0.00096768 '{snr=$2;downfact=$5;inttime_sec=timeres_sec*downfact;sigma_sim=sefd/sqrt(npol*bw_hz*inttime_sec);flu=snr*sigma_sim;print snr" -> "flu;}'
+
+# My derivations and why it is ok not to use downfact just use sigma (in fact S/N) as is in PRESTO output file is here : /home/msok/Desktop/EDA2/papers/2024/EDA2_FRBs/20250212_Scott_Ransom_downsampling.odt
+# basically this is because sigma = S/N = CONSTANT and corresponds to S/N calculated as peak_flux/sigma_native , where sigma_native is std. dev. of noise in the native time resolution dt. 
+# when downsampling is applied, it stays the same because of 1/SQRT(BOXCAR) "as long as the pulse is still resolved" - i.e. BOXCAR*dt <= PULSE_WIDTH
+# see also link below and Scott's comment copy pasted below here:
+# After discussion with Pratik and here:
+# https://github.com/scottransom/presto/blob/master/bin/single_pulse_search.py
+# if RMS is independent of downsampling I can just multiply by my original sigma_simulation - without taking into account downfact, hence downfact=1 below (vs. downfact=$5 in code above).
+# 
+# Scott Ransom :
+# The definition of "sigma" used is possibly slightly different from that used in other codes for S/N:
+#
+#    sigma = sum(signal-bkgd_level)/RMS/sqrt(boxcar_width)
+#
+# where the bkgd_level is typically 0 after detrending and RMS=1 after normalization. This definition has the advantage that you will get (basically) the same sigma for any pulse no
+# matter how much the input time series has been downsampled as long as the pulse is still resolved.
+# 
+cat ${files} | grep -v "#" | awk -v sefd=${sefd} -v npol=2 -v bw_hz=29629629 -v timeres_sec=0.00096768 '{snr=$2;inttime_sec=timeres_sec;sigma_sim=sefd/sqrt(npol*bw_hz*inttime_sec);flu=snr*sigma_sim;print snr" -> "flu;}'
