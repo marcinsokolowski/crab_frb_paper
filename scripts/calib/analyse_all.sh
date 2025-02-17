488d4b24e9720b72af8e738966bb26e458cf69c3
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-02-17 16:06:52 +0800

    added script for processing all nights

---
+++ scripts/calib/analyse_all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#!/bin/bash
+
+for dir in `ls -d 202?_??_??_pulsars_msok`
+do
+   echo 
+   fulldir=${dir}/J0534+2200_flagants_ch40_ch256/256/filterbank_msok_64ch 
+   if [[ -d ${fulldir} ]]; then
+      cd ${dir}
+      if [[ -d analysis ]]; then
+         echo "Data $dir alread processed -> skipped"
+      else
+         pwd
+         echo "Data $dir not processed yet -> processing now"
+         echo "~/github/crab_frb_paper/scripts/calib/process_dataset.sh > analysis.out 2>&1" > analysis!
+         chmod +x analysis!
+         echo "./analysis!"
+         ./analysis!
+      fi
+      cd ..
+   else
+      echo "WARNING : dataset $dir is incomplete (directory $fulldir does not exist)"
+   fi
+done
