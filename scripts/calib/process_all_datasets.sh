d9f84837425a58cf9bd47f2f0e3d7e5209fdbaf3
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-02-17 14:17:27 +0800

    corrections in "per hour"

--- scripts/calib/process_all_datasets.sh
+++ scripts/calib/process_all_datasets.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# cd /media/msok/5508b34c-040a-4dce-a8ff-2c4510a5d1a3/eda2/
+
+# calculate total time :
+total_time_sec=`grep "Time per file" 202?_??_??_pulsars_msok/analysis/updated.hdr | awk -v sum=0.00 '{sum+=$7;}END{print sum;}'`
+total_time_hour=`grep "Time per file" 202?_??_??_pulsars_msok/analysis/updated.hdr | awk -v sum=0.00 '{sum+=$7;}END{print sum/3600.00;}'`
+
 outdir=crab_full_analysis
 
 mkdir -p ${outdir}/merged/
@@ -21,6 +27,7 @@ echo "# UXTIME    #GPs" > ${outdir}/merged-ngps_vs_uxtime.txt
 for snrfile in `ls 202?_??_??_pulsars_msok/analysis/all_crab_gps_norfi.snr`
 do
    dir=`dirname $snrfile`
+   obsduration_hours=`cat ${dir}/updated.hdr | grep "Time per file" | awk '{print $6/3600.00;}'`
 
    cd $dir
    ux=`ls uxtime*txt | awk -v sum=0 -v count=0 '{ux=substr($1,7,10);sum+=ux;count+=1;}END{print sum/count;}'`
@@ -29,23 +36,23 @@ do
    count=`cat $snrfile | awk '{if($1!="#"){print $0;}}' | wc -l`
    count_merged=`cat $dir/merged/presto.cand_normal_snr | awk '{if($1!="#"){print $0;}}' | wc -l`
 
-   echo "$ux $count" >> ${outdir}/ngps_vs_uxtime.txt
-   echo "$ux $count_merged" >> ${outdir}/merged/merged-ngps_vs_uxtime.txt
+   echo "$ux $count" | awk -v obsduration=${obsduration_hours} '{print $1" "$2/obsduration;}' >> ${outdir}/ngps_vs_uxtime.txt
+   echo "$ux $count_merged" | awk -v obsduration=${obsduration_hours} '{print $1" "$2/obsduration;}' >> ${outdir}/merged/merged-ngps_vs_uxtime.txt
 done
 
 # PLOTTING :
 # NON-MERGED :
 cd ${outdir}
 # plot distribution of calibrated mean peak flux density :
-cp  ~/github/crab_frb_paper/scripts/root/FluDistrPowerLaw.C .
-root -l "FluDistrPowerLaw.C(\"all_crab_gps_norfi_fluxcal.singlepulse\")"
+cp  ~/github/crab_frb_paper/scripts/root/FluRatePerHourPowerLaw.C .
+root -l "FluRatePerHourPowerLaw.C(\"all_crab_gps_norfi_fluxcal.singlepulse\",${total_time_hour})"
 
-cp  ~/github/crab_frb_paper/scripts/root/SNRDistrPowerLaw.C .
-root -l "SNRDistrPowerLaw.C(\"all_crab_gps_norfi.snr\")"
+cp  ~/github/crab_frb_paper/scripts/root/SNRRatePerHourPowerLaw.C .
+root -l "SNRRatePerHourPowerLaw.C(\"all_crab_gps_norfi.snr\",${total_time_hour})"
 
 # plot disitrbution of luminosity :
 cp ~/github/crab_frb_paper/scripts/root/SpectralLuminosity_DistrPowerLaw.C .
-root -l "SpectralLuminosity_DistrPowerLaw.C(\"all_crab_gps_norfi_fluxcal.singlepulse\")"
+root -l "SpectralLuminosity_DistrPowerLaw.C(\"all_crab_gps_norfi_fluxcal.singlepulse\",${total_time_hour})"
 
 # plot N GPs vs. time :
 cp ~/github/crab_frb_paper/scripts/root/plot_ngps_vs_time.C .
@@ -55,15 +62,15 @@ root -l "plot_ngps_vs_time.C(\"ngps_vs_uxtime.txt\")"
 # PLOTTING MERGED :
 cd merged/
 # plot distribution of calibrated mean peak flux density :
-cp  ~/github/crab_frb_paper/scripts/root/FluDistrPowerLaw.C .
-root -l "FluDistrPowerLaw.C(\"presto_norfi_fluxcal.cand_normal\")"
+cp  ~/github/crab_frb_paper/scripts/root/FluRatePerHourPowerLaw.C .
+root -l "FluRatePerHourPowerLaw.C(\"presto_norfi_fluxcal.cand_normal\",${total_time_hour})"
 
 cp ~/github/crab_frb_paper/scripts/root/SpectralLuminosity_DistrPowerLaw.C .
-root -l "SpectralLuminosity_DistrPowerLaw.C(\"presto_norfi_fluxcal.cand_normal\")"
+root -l "SpectralLuminosity_DistrPowerLaw.C(\"presto_norfi_fluxcal.cand_normal\",${total_time_hour})"
 
 # plots SNR distribution 
-cp  ~/github/crab_frb_paper/scripts/root/SNRDistrPowerLaw.C .
-root -l "SNRDistrPowerLaw.C(\"presto.cand_normal_snr\")"
+cp  ~/github/crab_frb_paper/scripts/root/SNRRatePerHourPowerLaw.C .
+root -l "SNRRatePerHourPowerLaw.C(\"presto.cand_normal_snr\",${total_time_hour})"
 
 
 # plot N GPs vs. time :
