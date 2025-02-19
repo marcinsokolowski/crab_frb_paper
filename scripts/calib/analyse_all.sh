e1dd02a86762870b289a41252a65efed8f57c24e
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-02-19 16:16:46 +0800

    updated to output directory analysis_final

--- scripts/calib/analyse_all.sh
+++ scripts/calib/analyse_all.sh
@@ -8,12 +8,16 @@ do
    fulldir=${dir}/J0534+2200_flagants_ch40_ch256/256/filterbank_msok_64ch 
    if [[ -d ${fulldir} ]]; then
       cd ${dir}
-      if [[ -d analysis ]]; then
+      if [[ -d analysis_final ]]; then
          echo "Data $dir alread processed -> skipped"
       else
          pwd
          echo "Data $dir not processed yet -> processing now"
-         echo "~/github/crab_frb_paper/scripts/calib/process_dataset.sh > analysis.out 2>&1" > analysis!
+         
+         echo "mv analysis! analysis_old!"
+         mv analysis! analysis_old!
+         
+         echo "~/github/crab_frb_paper/scripts/calib/process_dataset.sh > analysis_final.out 2>&1" > analysis!
          chmod +x analysis!
          echo "./analysis!"
          ./analysis!
