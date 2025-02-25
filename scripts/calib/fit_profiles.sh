ca3ba88c5bfbfa38fbe72c276a69047c3dc14654
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-02-25 14:54:39 +0800

    added first tries of machine

--- scripts/calib/fit_profiles.sh
+++ scripts/calib/fit_profiles.sh
@@ -5,16 +5,22 @@ if [[ -n "$1" && "$1" != "-" ]]; then
    sefd=$1
 fi
 
-datfile=../_DM56.72.dat
+snr_threshold=40
 if [[ -n "$2" && "$2" != "-" ]]; then
-   datfile=$2
+   snr_threshold=$2
 fi
 
 
-echo "~/github/presto_tools/build/extract_pulses ${datfile} presto.txt -X 7937.64 -C -t 40.00 -P pulses_snr40_calibrated/ -r 100 -U 1"
-~/github/presto_tools/build/extract_pulses ${datfile} presto.txt -X 7937.64 -C -t 40.00 -P pulses_snr40_calibrated/ -r 100 -U 1
+datfile=../_DM56.72.dat
+if [[ -n "$3" && "$3" != "-" ]]; then
+   datfile=$3
+fi
 
-cd pulses_snr40_calibrated/
+
+echo "~/github/presto_tools/build/extract_pulses ${datfile} presto.txt -X 7937.64 -C -t $snr_threshold -P pulses_snr${snr_threshold}_calibrated/ -r 100 -U 1"
+~/github/presto_tools/build/extract_pulses ${datfile} presto.txt -X 7937.64 -C -t $snr_threshold -P pulses_snr${snr_threshold}_calibrated/ -r 100 -U 1
+
+cd pulses_snr${snr_threshold}_calibrated/
 
 cp ~/github/crab_frb_paper/scripts/root/plot_psr_profile.C .
 
@@ -25,3 +31,4 @@ do
 done
 
 cat *.refit | awk '{print $9;}' > tau.txt
+root -l "histofile.C(\"tau.txt\",0,1,0,0.01)"
