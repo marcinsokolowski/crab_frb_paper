a3bb40b2348c843b45b81f0978e1829ba0b8cd16
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-04-28 08:20:37 +0800

    corrections and parameters

--- scripts/paper/mc_gp_poisson.py
+++ scripts/paper/mc_gp_poisson.py
@@ -1,7 +1,9 @@
 # https://numpy.org/doc/2.2/reference/random/generated/numpy.random.poisson.html
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 debug = False
 
@@ -11,38 +13,59 @@ period = 0.0333924123
 time_resolution = (1.08*64*14)/1000000.00 # as time resolution of my data
 n_samples = int( obstime / time_resolution )
 
-# Lambda = 0.033
-Lambda = period*(1.00/500.00)
-n_samples = int( obstime / Lambda )
-
-s = np.random.poisson(Lambda, int(n_samples))
-print("DEBUG : s[0] = %.6f , s[1] = %.6f" % (s[0],s[1]))
-count, bins, ignored = plt.hist(s, 10, log=True ) # , density=True)
-
-plt.show()
-
-# time_between_pulses = np.zeros( n_samples-1 )
-time_between_pulses = []
-non_zero=0
-for i in range(0,n_samples) :
-   if s[i] > 0 :
-      if debug : 
-         print("%d : %.8f" % (i,s[i]))
-      non_zero += 1
-
-print("Number of non-zero bins %d vs. all bins = %d" % (non_zero,len(s)))      
-
-prev_i = -1
-for i in range(0,n_samples) :
-   if s[i] > 0 :
-      if prev_i >= 0 :         
-         diff = (i - prev_i)*time_resolution
-         time_between_pulses.append(diff)
-   
-      prev_i = i 
+if __name__ == '__main__':   
+   Lambda = period*(1.00/500.00)
+   if len(sys.argv) > 1 and sys.argv[1] != "-" :
+      Lambda = float(sys.argv[1])
+   n_samples = int( obstime / Lambda )    
+ 
+   oversampling_factor = 1
+   if len(sys.argv) > 2 and sys.argv[2] != "-" :
+      oversampling_factor = float(sys.argv[2])
       
-count2, bins2, ignored2 = plt.hist(time_between_pulses, 50, log=True )      
-plt.show()
+   n_samples = int(n_samples*oversampling_factor)
+
+   print("###################################")
+   print("PARAMETERS:")   
+   print("###################################")
+   print("n_samples = %d" % (n_samples))
+   print("###################################")
+
+   outfile=("time_diff_Lambda_%.8f.txt" % (Lambda))
+   
+   s = np.random.poisson(Lambda, int(n_samples))
+   print("DEBUG : s[0] = %.6f , s[1] = %.6f" % (s[0],s[1]))
+   count, bins, ignored = plt.hist(s, 10, log=True ) # , density=True)
+
+   plt.show()
+
+   # time_between_pulses = np.zeros( n_samples-1 )
+   time_between_pulses = []
+   non_zero=0
+   for i in range(0,n_samples) :
+      if s[i] > 0 :
+         if debug : 
+            print("%d : %.8f" % (i,s[i]))
+         non_zero += 1
+
+   print("Number of non-zero bins %d vs. all bins = %d" % (non_zero,len(s)))      
+
+   outf = open(outfile,"w")
+   prev_i = -1
+   for i in range(0,n_samples) :
+      if s[i] > 0 :
+         if prev_i >= 0 :         
+            diff = (i - prev_i)*time_resolution
+            time_between_pulses.append(diff)
+            line=("%.8f\n" % diff)
+            outf.write(line)
+   
+         prev_i = i 
+   
+   outf.close()      
+       
+   count2, bins2, ignored2 = plt.hist(time_between_pulses, 50, log=True )      
+   plt.show()
 
 
 # pulse_times = np.zeros(len(s))
