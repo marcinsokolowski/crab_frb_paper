a6bea8b920e7fe1e030c95a937ec0409ab247d35
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-05-19 16:42:58 +0800

    corrections

--- scripts/python/viss.py
+++ scripts/python/viss.py
@@ -1,10 +1,16 @@
 import math
+import sys
+
+l=2000 # pc
+if len(sys.argv) > 1:
+   l = float( sys.argv[1] )
+
 
 # theta_arcsec=0.00965971 # arcsec ~9.6 mas
 theta_arcsec2=0.0009331
 theta_arcsec=math.sqrt(theta_arcsec2)
 theta_rad=theta_arcsec/206265.00
-l=2000 # pc
+print("theta = %.8f [arcsec] = %e [rad]" % (theta_arcsec,theta_rad))
 dt=75*86400 # /2.00 # has to be ~ Half-Peak (see ACF analysis)
 pc=3.0857*1e13 # km
 
