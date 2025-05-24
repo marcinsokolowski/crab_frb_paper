25b5c74694f337bf9a8089c8fa896997c8b8db22
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-05-24 10:17:36 +0800

    initial version

---
+++ scripts/python/third_order_eq.py
@@ -0,0 +1,87 @@
+import math
+import cmath
+import sys
+import numpy
+
+def solve_cubic_equation(a, b, c, d):
+    delta = (18*a*b*c*d) - (4*(b**3)*d) + ((b**2)*(c**2)) - (4*a*(c**3)) - (27*(a**2)*(d**2))
+    if delta > 0:
+        num_roots = 3
+    elif delta == 0:
+        num_roots = 2
+    else:
+        num_roots = 1
+
+    if num_roots == 3:
+        y1 = (((-b**3) / (27*(a**3))) + ((b*c) / (6*(a**2))) - (d / (2*a))) + cmath.sqrt((((-b**3) / (27*(a**3))) + ((b*c) / (6*(a**2))) - (d / (2*a)))**2 + ((c / (3*a)) - ((b**2) / (9*(a**2))))**3)
+        y2 = (((-b**3) / (27*(a**3))) + ((b*c) / (6*(a**2))) - (d / (2*a))) - cmath.sqrt((((-b**3) / (27*(a**3))) + ((b*c) / (6*(a**2))) - (d / (2*a)))**2 + ((c / (3*a)) - ((b**2) / (9*(a**2))))**3)
+        
+        x1 = (y1**(1/3)) + (y2**(1/3)) - (b / (3*a))
+        x2 = ((-1/2)*((y1**(1/3)) + (y2**(1/3)))) - (b / (3*a)) + ((1/2)*1j*cmath.sqrt(3)*((y1**(1/3))-(y2**(1/3))))
+        x3 = ((-1/2)*((y1**(1/3)) + (y2**(1/3)))) - (b / (3*a)) - ((1/2)*1j*cmath.sqrt(3)*((y1**(1/3))-(y2**(1/3))))
+        return x1, x2, x3
+
+    if num_roots == 2:
+        x1 = ((-b + cmath.sqrt(b**2-(4*a*c))) / (2*a))
+        x2 = ((-b - cmath.sqrt(b**2-(4*a*c))) / (2*a))
+        return x1, x2
+    
+    if num_roots == 1:
+        x1 = (-d/c)
+        return x1
+        
+if __name__ == '__main__':        
+   t_ms=5.00
+   if len(sys.argv) > 1:
+      t_ms = float( sys.argv[1] )
+   
+   t_riss=51 # days
+   if len(sys.argv) > 2:
+      t_riss = float( sys.argv[2] )
+
+   v_sc=15
+   if len(sys.argv) > 3:
+      v_sc = float( sys.argv[3] )
+
+   
+   v_psr = 120
+   v_e = 30
+   D=2000 # pc
+
+   alpha = 1574.848324697*math.sqrt(t_ms)/t_riss
+   delta1 = v_psr - v_sc
+   delta2 = v_e - v_sc
+
+   a = alpha 
+   b = (delta1 - delta2)
+   c = -alpha*D
+   d = D*delta2
+
+   # solve equation : ay^3 + by^2 + cy + d = 0 
+   print("a = %.8f , b = %.8f, c = %.8f , d = %.8f" % (a,b,c,d))
+   (x1,x2,x3) = solve_cubic_equation(a, b, c, d)
+   print(f"The roots of the cubic equation are:")
+   print(f"x1 = {x1}")
+   print(f"x2 = {x2}")
+   print(f"x3 = {x3}")
+
+   print("\n\nPhysical solutions:")   
+   good=0
+   physical_solutions=[]
+   for x in (x1,x2,x3) :   
+      x_im = numpy.imag(x)
+      x_re = numpy.real(x)   
+      d  = 0.00
+      if math.fabs(x_im) < 1e-10 and x_re >= 0 :    
+         d = x_re*x_re
+         x_d = d/(D-d)
+         good = good + 1
+         physical_solutions.append(d)
+         print("Physical solution %d = %.8f pc (x = %.8f)" % (good,d,x_d))
+      
+
+
+
+   
+   
+
