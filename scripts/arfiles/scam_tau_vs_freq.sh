830e4b20cfc0aa601c3e46626ade55e1dfc3ea5a
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-05-29 15:13:47 +0800

    added script to run SCAMP

---
+++ scripts/arfiles/scam_tau_vs_freq.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+template="/media/msok/f5ce6064-9dc3-40c3-bc9c-7314e8594519/eda2/202?_??_??_pulsars_msok/J0534+2200_flagants_ch40_ch256/256/single_pulse_archive_pulse_*sec_interval*sec/"
+if [[ -n "$1" && "$1" != "-" ]]; then
+   template="$1"
+fi
+
+path=`pwd`
+
+for dataset in `ls -d $template`
+do
+   cd $dataset
+   mkdir -p scamp
+   cd scamp
+   
+   echo "cp ../*.ar ."
+   cp ../*.ar .
+   
+   arfile=`ls *.ar | tail -1`
+   ln -s /home/msok/github/psrchive/More/python/.libs/_psrchive.so
+   
+   echo "python ~/github/SCAMP_I/create_config_ascii_from_archive.py -f $arfile -d ./"
+   python ~/github/SCAMP_I/create_config_ascii_from_archive.py -f $arfile -d ./
+   
+   ascii_file=${arfile}.4ch.ascii
+   csv_file=J0534+2200_config_4ch.csv
+   
+   echo "~/github/SCAMP_I/run.sh $ascii_file $csv_file"
+   ~/github/SCAMP_I/run.sh $ascii_file $csv_file    
+   
+   cd $path
+done
