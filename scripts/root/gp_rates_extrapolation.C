fac9cc181c386569d451e1ab1579dee0136e9e7f
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2025-06-07 16:44:45 +0800

    corrections

--- scripts/root/gp_rates_extrapolation.C
+++ scripts/root/gp_rates_extrapolation.C
@@ -1,5 +1,6 @@
 
-void gp_rates_extrapolation( double l=1e38 )
+// see /home/msok/Desktop/EDA2/papers/2024/EDA2_FRBs/20250531_number_of_galaxies.odt
+void gp_rates_extrapolation( double l=1e38, int n_crabs_per_galaxy=1, double n_galaxies=1e8 )
 {
    double n_ref = 5208;
    double alpha = -3.004;
@@ -16,5 +17,12 @@ void gp_rates_extrapolation( double l=1e38 )
    printf("Hour rate for l = %e [erg/Hz/s] = %e [GPs/hour]\n",l,hour_rate);
    printf("1 year in hours = %.8f\n",year_in_hours);
    printf("Wait time = %e years vs. Hubble time = %e years (in hours = %e)\n",wait_time_for_one_years,hubble_time,wait_time_for_one_hours);
+
+
+   // z <= 0.1 universe :
+   double wait_time_from_local_universe = wait_time_for_one_years/(n_galaxies*n_crabs_per_galaxy);
+   double wait_time_from_local_universe_hours = wait_time_from_local_universe*(365.00*24.00);
+   double wait_time_from_local_universe_months = wait_time_from_local_universe*12;
+   printf("Wait times in the local universe (z<0.1) = %e years = %.4f hours = %.4f months\n",wait_time_from_local_universe,wait_time_from_local_universe_hours,wait_time_from_local_universe_months);
 
 }
